c8240810061f3cf86d6094c506e332cd68fad4eb
Tadeusz Miszczyk <42252259+8tm@users.noreply.github.com>
2019-04-10 22:42:08 +0200

    Little change

--- python/tools/check_connections.py
+++ python/tools/check_connections.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-""" Tool - send email about lost connections"""
-
 # ----------------------------------------------------------------------------------------------------------------------
+
+""" Tool - send email about lost connections"""
 
 __author__ = "Tadeusz Miszczyk"
 __version__ = "1.0.0"
